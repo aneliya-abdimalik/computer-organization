0b034332925609f9193d17ac53b0a865a4d5e00a
Aneliya <97451473+aneliya-abdimalik@users.noreply.github.com>
2025-07-09 12:36:14 +0300

    Create farm.c

---
+++ attack lab/farm.c
@@ -0,0 +1,111 @@
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned getval_445()
+{
+    return 3268839752U;
+}
+
+unsigned getval_166()
+{
+    return 3285616968U;
+}
+
+void setval_159(unsigned *p)
+{
+    *p = 2429419848U;
+}
+
+void setval_248(unsigned *p)
+{
+    *p = 3284306760U;
+}
+
+unsigned addval_167(unsigned x)
+{
+    return x + 3287779656U;
+}
+
+void setval_319(unsigned *p)
+{
+    *p = 3285058888U;
+}
+
+unsigned addval_144(unsigned x)
+{
+    return x + 3285057864U;
+}
+
+void setval_194(unsigned *p)
+{
+    *p = 3281012871U;
+}
+
+void setval_128(unsigned *p)
+{
+    *p = 2425393496U;
+}
+
+unsigned getval_388()
+{
+    return 3263776865U;
+}
+
+unsigned getval_383()
+{
+    return 3285617992U;
+}
+
+unsigned addval_459(unsigned x)
+{
+    return x + 3287779656U;
+}
+
+void setval_469(unsigned *p)
+{
+    *p = 2425378983U;
+}
+
+void setval_187(unsigned *p)
+{
+    *p = 3251503432U;
+}
+
+unsigned addval_475(unsigned x)
+{
+    return x + 3599321214U;
+}
+
+void setval_322(unsigned *p)
+{
+    *p = 3284306248U;
+}
+
+void setval_256(unsigned *p)
+{
+    *p = 2445445448U;
+}
+
+void setval_363(unsigned *p)
+{
+    *p = 3281031256U;
+}
+
+void setval_446(unsigned *p)
+{
+    *p = 3287779658U;
+}
+
+unsigned getval_360()
+{
+    return 3287746888U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
